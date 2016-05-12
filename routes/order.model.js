54c7e3eb6e6b3fcabd3afb95610be7b3c9bc5da0
dong yuangui <dongyuangui@appadhoc.com>
2016-05-12 13:34:06 +0800

    tostring

--- routes/order.model.js
+++ routes/order.model.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var OrderSchema = new Schema({
-  openid: Number,
+  openid: String,
   ordertime:{type : Date, default: Date.now}
 });
 
