fe1e165256ef636ebbc9555ca1f2d1f31f5fe7aa
dong yuangui <dongyuangui@appadhoc.com>
2016-05-12 22:01:19 +0800

    use mongodb ref

--- routes/order.model.js
+++ routes/order.model.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var OrderSchema = new Schema({
-  openid: {type : String,ref:'User'}
+  openid: {type : String,ref:'User'},
   ordertime:{type : Date, default: Date.now}
 });
 
